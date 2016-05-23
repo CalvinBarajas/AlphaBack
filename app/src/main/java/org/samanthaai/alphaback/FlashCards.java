70d86e16d63d9e0b940aa25a7e169905a039f898
Calvin Barajas <CalvinBarajas@AIM.com>
2016-05-22 22:06:26 -0700

    continued cleaning up comments, variables, added annotation code against NULL values, etc.

--- app/src/main/java/org/samanthaai/alphaback/FlashCards.java
+++ app/src/main/java/org/samanthaai/alphaback/FlashCards.java
@@ -7,159 +7,93 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class FlashCards extends AppCompatActivity {
 
     // global variables
-    private static String[] englishAlphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}; // this is the entire english alphabet
-    private static LinearLayout colorBackground;
+    private static String[] englishAlphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}; // english alphabet
     private static int displayedLetterIndex;
-
-
-
-    SoundPool mySoundPool;  // create and configure a SoundPool instance
+    private static SoundPool mySoundPool;  // create and configure a SoundPool instance
     private static int soundBtnPress;  // answer is correct
-
-
-
-
-
-
-
-
-
-
+    private static TextView largeLetter; // letter displayed at the center of flash card
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_cards);
 
-
-        // to create sound for game
+        // call method to initialize game sound
         initializeSoundPool();
 
+        // find view for "large letter" (center of flash card)
+        largeLetter = (TextView) findViewById(R.id.large_letter);
 
-        // initialize displayedLetterIndex variable
+        // set font style for "large letter" to something easy to read
+        Typeface customFont = Typeface.createFromAsset(getAssets(), "fonts/acme.ttf");
+        largeLetter.setTypeface(customFont);
+
+        // set initial letter for "large letter" to letter "z"
         displayedLetterIndex = 25;
-
-        // find view in order to change background color of activity_flash_cards.xml
-        colorBackground = (LinearLayout) findViewById(R.id.flash_cards_bg);
-
-        // find the view for LARGE LETTER
-        final TextView largeLetter = (TextView) findViewById(R.id.large_letter);
-
-        // change font style for LARGE LETTER
-        Typeface myCustomFont3 = Typeface.createFromAsset(getAssets(), "fonts/acme.ttf");
-        largeLetter.setTypeface(myCustomFont3);
-
-        // set initial letter for LARGE LETTER
         largeLetter.setText(englishAlphabet[displayedLetterIndex]);
 
-        // find view for NEXT BUTTON
+        // find view for "next" button
         Button nextButton = (Button) findViewById(R.id.next_btn);
 
-        // assign listener to NEXT BUTTON
+        // assign listener to "next" button
+        assert nextButton != null;
         nextButton.setOnClickListener(new View.OnClickListener() {
+            @NonNull
             @Override
             public void onClick(View v) {
+                mySoundPool.play(soundBtnPress, 1.0f, 1.0f, 1, 0, 1); // play sound
 
-                mySoundPool.play(soundBtnPress, 1.0f, 1.0f, 1, 0, 1);
-
-                // if index is already at letter Z (25), increment by 1 (26)
                 if (displayedLetterIndex <= 25) {
-                    displayedLetterIndex += 1;
-                }
+                    displayedLetterIndex += 1; } // if index is already at letter "z" (25), increment by 1 (26)
 
-                // if index is at 26, reset variable to A (0)
                 if (displayedLetterIndex > 25) {
-                    displayedLetterIndex = 0;
+                    displayedLetterIndex = 0; // if index is at 26, assign variable to "a" (index 0)
                     Toast toast = Toast.makeText(getApplicationContext(), "Starting At The Beginning", Toast.LENGTH_SHORT);
-                    toast.show();
-                }
+                    toast.show(); }
 
                 largeLetter.setText(englishAlphabet[displayedLetterIndex]);
             }
         });
 
-
-
-        // find view for PREV BUTTON
+        // find view for "prev" button
         Button prevButton = (Button) findViewById(R.id.prev_btn);
 
-        // assign listener to PREV BUTTON
+        // assign listener to "prev" button
+        assert prevButton != null;
         prevButton.setOnClickListener(new View.OnClickListener() {
+            @NonNull
             @Override
             public void onClick(View v) {
+                mySoundPool.play(soundBtnPress, 1.0f, 1.0f, 1, 0, 1); // play sound
 
-                mySoundPool.play(soundBtnPress, 1.0f, 1.0f, 1, 0, 1);
-
-                // if index is already at letter Z (25), increment by 1 (26)
                 if (displayedLetterIndex >= 0) {
-                    displayedLetterIndex -= 1;
-                }
+                    displayedLetterIndex -= 1; }// if index is already at letter "a" (index 0), decrement by 1 (-1)
 
                 if (displayedLetterIndex < 0) {
-                    displayedLetterIndex = 25;
+                    displayedLetterIndex = 25; // if index is at -1, assign variable to "z" (index 25)
                     Toast toast = Toast.makeText(getApplicationContext(), "Starting At The End", Toast.LENGTH_SHORT);
-                    toast.show();
-                }
+                    toast.show(); }
 
                 largeLetter.setText(englishAlphabet[displayedLetterIndex]);
             }
         });
-
-
-
-
-
     }
 
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.e("onPause() -->", "The onPause() Event GuessingGame.java");
-
-        Intent intent = new Intent(FlashCards.this, MainActivity.class);
-        startActivity(intent);
-
-
-        finish();
-        Log.e("B5-001 FINISH()---> ", "DID FINISH() EXECUTE?");
-
-
-    }
-
-
-
-
-
-
-
-
-
-
+    // sound pool method
     private void initializeSoundPool() {
-
-        ///////////////////////////////////////////////////////////////////////
-        ///////////////////// Begin - SoundPool Example ///////////////////////
-        ///////////////////////////////////////////////////////////////////////
-
         // check the API version in order to use the correct code
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-
             //code greater or equal to API 21 (lollipop)
-            Log.e("VER.SDK_INT --->", Build.VERSION.SDK_INT + "" );
-            Log.e("VER_CODES.LOLLIPOP --->", Build.VERSION_CODES.LOLLIPOP + "" );
-
             AudioAttributes audioAttributes = new AudioAttributes.Builder()
                     .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
                     .setUsage(AudioAttributes.USAGE_GAME)
@@ -170,51 +104,24 @@ public class FlashCards extends AppCompatActivity {
                     .setAudioAttributes(audioAttributes)
                     .build();
 
-            soundBtnPress = mySoundPool.load(this, R.raw.right, 1);
+            soundBtnPress = mySoundPool.load(this, R.raw.right, 1); // only one sound needed
 
         } else {
-            Log.e("Does This Execute --->", "NoNoNoNoNoNo" );
             //code for all other versions
             // for older API versions (i.e., API less than 21)
             mySoundPool = new SoundPool(2, AudioManager.STREAM_MUSIC, 1);
             soundBtnPress = mySoundPool.load(this, R.raw.right, 1);
         }
-
-
-
-        ///////////////////////////////////////////////////////////////////////
-        ///////////////////// End - SoundPool Example ///////////////////////
-        ///////////////////////////////////////////////////////////////////////
-
-
-
-
-
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
 
+        // return to home page
+        Intent intent = new Intent(FlashCards.this, MainActivity.class);
+        startActivity(intent);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        finish();
+    }
 }
--- app/src/main/java/org/samanthaai/alphaback/MainActivity.java
+++ app/src/main/java/org/samanthaai/alphaback/MainActivity.java
@@ -3,8 +3,8 @@ package org.samanthaai.alphaback;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,74 +16,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        // find the view (used for ending the game)
+        // find view for "quit" button (end game)
         Button endGame = (Button) findViewById(R.id.end_game_button);
 
         // assign listener to button (used for ending the game)
+        assert endGame != null;
         endGame.setOnClickListener(new View.OnClickListener() {
+            @NonNull
+            @Override
             public void onClick(View view) {
                 finish();
             }
         });
 
+        // find view for "guessing game" start button
+        Button startGuessingGame = (Button) findViewById(R.id.guessing_game_btn);
 
-        // find the view
-        Button startGame = (Button) findViewById(R.id.guessing_game_btn);
-
-        // assign listener to button
-        startGame.setOnClickListener(new View.OnClickListener() {
-
+        // assign listener to "guessing game" start button
+        assert startGuessingGame != null;
+        startGuessingGame.setOnClickListener(new View.OnClickListener() {
+            @NonNull
+            @Override
             public void onClick(View view) {
-
+                // launch GuessingGame activity
                 Intent intent = new Intent(MainActivity.this, GuessingGame.class);
                 startActivity(intent);
             }
-
-
         });
 
+        // find view for home page header text and apply custom font
+        TextView textView = (TextView) findViewById(R.id.homepage_header_tv);
+        Typeface customFont = Typeface.createFromAsset(getAssets(), "fonts/school.ttf");
+        textView.setTypeface(customFont);
 
-        // find the view
-        TextView textView = (TextView) findViewById(R.id.hello_world);
-        Typeface myCustomFont1 = Typeface.createFromAsset(getAssets(), "fonts/school.ttf");
-        textView.setTypeface(myCustomFont1);
-
-
-
-        // find the view for the FLASH CARDS BUTTON
+        // find view for the flash cards start button
         Button flashCardsBtn = (Button) findViewById(R.id.flash_cards_btn);
 
         // assign listener to FLASH CARDS BUTTON and launch activity
+        assert flashCardsBtn != null;
         flashCardsBtn.setOnClickListener(new View.OnClickListener() {
-
+            @NonNull
+            @Override
             public void onClick(View view) {
-
+                // launch FlashCards activity
                 Intent intent = new Intent(MainActivity.this, FlashCards.class);
                 startActivity(intent);
             }
-
-
         });
-
-
-
-
-
-
     }
 
     @Override
     protected void onPause() {
-         super.onPause();
-        Log.e("onPause() -->", "The onPause() Event MainActivity.java");
+        super.onPause();
         finish();
     }
-
-
-
-
 }
-
-
-// replace all hard-coded text with variables
